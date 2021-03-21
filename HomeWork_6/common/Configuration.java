0f12ff3d039f54faf5886d38688694c8dd37020d
doppel78 <68333251+doppel78@users.noreply.github.com>
2021-03-21 15:42:06 +0300

    Delete Configuration.java

--- HomeWork_6/common/Configuration.java
+++
@@ -1,8 +0,0 @@
-package HomeWork_6.common;
-
-public class Configuration {
-    public static final String BASE_URL = "https://crm.geekbrains.space/";
-    public static final String LOGIN_PATH = "user/login";
-    public static final String STUDENT_LOGIN = "Applanatest";
-    public static final String STUDENT_PASSWORD = "Student2020!";
-}
